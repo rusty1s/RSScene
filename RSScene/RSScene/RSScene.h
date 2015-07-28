cde6db51ac8a0939e26e44b2d3dd695601432962
Matthias Fey <matthias.fey@tu-dortmund.de>
2015-07-28 20:09:15 +0200

    Setup workspace

---
+++ RSScene/RSScene/RSScene.h
@@ -0,0 +1,19 @@
+//
+//  RSScene.h
+//  RSScene
+//
+//  Created by Matthias Fey on 28.07.15.
+//  Copyright © 2015 Matthias Fey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RSScene.
+FOUNDATION_EXPORT double RSSceneVersionNumber;
+
+//! Project version string for RSScene.
+FOUNDATION_EXPORT const unsigned char RSSceneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSScene/PublicHeader.h>
+
+
